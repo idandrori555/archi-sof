9c20e97bb1def025c5c91b192efc4a82cc85e620
idan <idandrori555@gmail.com>
2026-03-03 17:07:03 +0200

    redid VS and revised INJECTOR for 'notepad'

---
+++ DLL/Project1/lib.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+
+// ENTRY POINT FOR DLL (WINAPI)
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Show MessageBox
+        MessageBoxA(NULL, "Running Inside Notepad!", "INJECTED!", MB_OK | MB_ICONEXCLAMATION);
+        break;
+
+    case DLL_THREAD_ATTACH:
+        break;
+
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
